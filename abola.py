15ac2e04e3726c7d793fb57e19da8e8f5aeeb807
Joni Batista <jonibatista@gmail.com>
2012-10-17 18:55:44 +0100

    done! waiting for bugs...

--- abola.py
+++ abola.py
@@ -3,9 +3,11 @@ import json
 import os
 import datetime
 from datetime import timedelta
+import urllib.request
 
 ## DEF CONSTANTS
-G_SRC_FOLDER = "C:/Users/INOV2073/Documents/abola" #"/Users/jbatista/Pictures/abola/"
+G_SRC_FOLDER = "C:/Users/INOV2073/Documents/abola/" #"/Users/jbatista/Pictures/abola/"
+G_DAYS_OF_WEEK = ['wseg', 'wter', 'wqua', 'wqui', 'wset', 'wsab', 'wdom']
 
 ## load configuration from file
 file = open('config.txt', 'r') #specify file to open
@@ -44,7 +46,20 @@ today = datetime.date.today()
 i = 7 # the abola newspaper only allow us to backward 7 days  
 while(today > lastDate and i > 0):
 	
-	print ("Downloading abola newspaper cover of ", str(today), "...")
+	print ("Downloading abola newspaper cover of " + str(today) + "...")
+
+	# build the URL of the cover link
+	url = 'http://www.abola.pt/' + G_DAYS_OF_WEEK[today.weekday()] + '/wfotosdia/wdiag.jpg'
+
+	# create desdination filename 'YYYYMMDD.jpeg'
+	filename = str(today.year) + str(today.month) + str(today.day)  + ".jpeg"
+        
+	# create destination file
+	print (G_SRC_FOLDER + filename)
+	f = open(G_SRC_FOLDER + filename, 'wb')
+	f.write(urllib.request.urlopen(url).read())
+	f.close()
+	
 	# done, lets go back one more day
 	today -= timedelta(days=1)
 	i -=1
