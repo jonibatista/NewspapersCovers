0a55ae8d0ed27157a99c32fedb73555778fec419
Joni Batista <jonibatista@gmail.com>
2012-10-17 17:04:36 +0100

    Srouce code ported to python 3.3

--- abola.py
+++ abola.py
@@ -4,6 +4,9 @@ import os
 import datetime
 from datetime import timedelta
 
+## DEF CONSTANTS
+G_SRC_FOLDER = "C:/Users/INOV2073/Documents/abola" #"/Users/jbatista/Pictures/abola/"
+
 ## load configuration from file
 file = open('config.txt', 'r') #specify file to open
 
@@ -18,7 +21,7 @@ for item in file.readlines():
 
 
 ## get last cover downloaded
-listCovers = os.listdir('/Users/jbatista/Pictures/abola/')
+listCovers = os.listdir(G_SRC_FOLDER)
 listCovers.sort()
 lastDownload = ""
 
@@ -32,18 +35,18 @@ month = int(lastDownload)%10000 #yyyy.MMDD
 month = int(month)/100 #MM.dd
 day = int(lastDownload)%100 #yyyymm.DD
 
-lastDate = datetime.date(year, month, day)
+lastDate = datetime.date(int(year), int(month), int(day))
 
 ## get the curent date
-timestamp = datetime.datetime.now()
-now = datetime.date(timestamp.year, timestamp.month, timestamp.day)
+today = datetime.date.today()
+#now = datetime.date(today.year, today.month, today.day)
 
 i = 7 # the abola newspaper only allow us to backward 7 days  
-while(now > lastDate and i > 0):
+while(today > lastDate and i > 0):
 	
-	print "Downloading abola newspaper cover of ", str(now), "..."
+	print ("Downloading abola newspaper cover of ", str(today), "...")
 	# done, lets go back one more day
-	now -= timedelta(days=1)
+	today -= timedelta(days=1)
 	i -=1
 
 
