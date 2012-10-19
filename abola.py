040b25764966b4abcf8aa80ac8e911c01d4ea2f6
Jóni Batista <batista.joni@gmail.com>
2012-10-19 23:58:03 +0100

    Issue #1 fixed

--- abola.py
+++ abola.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+#!/Library/Frameworks/Python.framework/Versions/3.3/bin/python3.3
+
+
 import json
 import os
 import datetime
@@ -6,7 +8,7 @@ from datetime import timedelta
 import urllib.request
 
 ## DEF CONSTANTS
-G_SRC_FOLDER = "C:/Users/INOV2073/Documents/abola/" #"/Users/jbatista/Pictures/abola/"
+G_SRC_FOLDER =  "/Users/jbatista/Pictures/abola/"
 G_DAYS_OF_WEEK = ['wseg', 'wter', 'wqua', 'wqui', 'wset', 'wsab', 'wdom']
 
 ## load configuration from file
@@ -24,29 +26,45 @@ for item in file.readlines():
 
 ## get last cover downloaded
 listCovers = os.listdir(G_SRC_FOLDER)
-listCovers.sort()
 lastDownload = ""
 
 ## get the date of the last cover
-for i in listCovers[-1]:
-	if i.isdigit():
-		lastDownload = lastDownload + i
+if len(listCovers) > 0:
 
-year = abs(int(lastDownload)/10000) #YYYY.mmdd
-month = int(lastDownload)%10000 #yyyy.MMDD
-month = int(month)/100 #MM.dd
-day = int(lastDownload)%100 #yyyymm.DD
+	listCovers.sort()
+	for i in listCovers[-1]:
+		if i.isdigit():
+			lastDownload = lastDownload + i
 
-lastDate = datetime.date(int(year), int(month), int(day))
+
+	try:
+		year = int(lastDownload)/10000 #YYYY.mmdd
+		month = int(lastDownload)%10000 #yyyy.MMDD
+		month = int(month)/100 #MM.dd
+		day = int(lastDownload)%100 #yyyymm.DD
+
+		lastDate = datetime.date(int(year), int(month), int(day))
+
+	except:
+		#download all covers, a complete week....
+		lastDate = datetime.date.today() - timedelta(days=7)
+
+else:
+		#download all covers, a complete week....
+		lastDate = datetime.date.today() - timedelta(days=7)
+
 
 ## get the curent date
 today = datetime.date.today()
 #now = datetime.date(today.year, today.month, today.day)
 
+if today == lastDate:
+	print("Nothing new. No covers to download....")
+
 i = 7 # the abola newspaper only allow us to backward 7 days  
 while(today > lastDate and i > 0):
 	
-	print ("Downloading abola newspaper cover of " + str(today) + "...")
+	print ("Downloading abola new spaper cover of " + str(today) + "...")
 
 	# build the URL of the cover link
 	url = 'http://www.abola.pt/' + G_DAYS_OF_WEEK[today.weekday()] + '/wfotosdia/wdiag.jpg'
@@ -55,13 +73,16 @@ while(today > lastDate and i > 0):
 	filename = str(today.year) + str(today.month) + str(today.day)  + ".jpeg"
         
 	# create destination file
-	print (G_SRC_FOLDER + filename)
 	f = open(G_SRC_FOLDER + filename, 'wb')
+
+	# download image and write it HDD
 	f.write(urllib.request.urlopen(url).read())
 	f.close()
+
+	print ("done!")
 	
 	# done, lets go back one more day
 	today -= timedelta(days=1)
 	i -=1
 
-
+# EDN OF SCRIPT
