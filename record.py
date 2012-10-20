27a90a945463e9486301e3d2d94f9ae9c715ba06
Jóni Batista <batista.joni@gmail.com>
2012-10-20 15:20:41 +0100

    Create the algorithm that identifies which covers will be downloaded and their position (x, y) in the table of the  html web page.

--- record.py
+++ record.py
@@ -1,17 +1,23 @@
-#!/usr/bin/python
+#!/Library/Frameworks/Python.framework/Versions/3.3/bin/python3.3
+
 import json
 import os
 import datetime
 from datetime import timedelta
 import urllib.request
+import sys
+
 
 ## DEF CONSTANTS
-G_SRC_FOLDER = "C:/Users/INOV2073/Documents/record/" #"/Users/jbatista/Pictures/abola/"
+G_SRC_FOLDER =  "/Users/jbatista/Pictures/record/"
 G_ROWS_NUMBER = 3
 G_COLUMNS_NUMBER = 4
 G_TOTAL_PAGES = 10
 G_TOTAL_COVERS_PER_PAGE = G_ROWS_NUMBER * G_COLUMNS_NUMBER
 
+
+
+## BEGIN
 
 # Each html page have X newspaper covers, where x = G_COLUMNS_NUMBER * G_ROWS_NUMBER.
 # This is represented by a html table with G_ROWS_NUMBER rows and G_COLUMNS_NUMBER columns.
@@ -20,11 +26,15 @@ G_TOTAL_COVERS_PER_PAGE = G_ROWS_NUMBER * G_COLUMNS_NUMBER
 coversPerPage = [ [ 0 for i in range(G_COLUMNS_NUMBER) ] for j in range(G_ROWS_NUMBER) ]
 
 ## get last cover downloaded
-listCovers = os.listdir(G_SRC_FOLDER) 
+try:
+    listCovers = os.listdir(G_SRC_FOLDER) 
+except:
+    print("Error! ", sys.exc_info()[0])
+    exit()
 
 if len(listCovers) == 0:
     # directory is empty. We have to make a full download since page G_TOTAL_PAGES
-    # starting at X days ago...
+    # starting at X days ago... Full download.
     lastDownload = datetime.date.today() - timedelta(days=(G_TOTAL_PAGES*G_TOTAL_COVERS_PER_PAGE-1))
     startPage = G_TOTAL_PAGES
 else:
@@ -35,26 +45,33 @@ else:
             if i.isdigit():
                     lastDownload = lastDownload + i
 
-    year = abs(int(lastDownload)/10000) #YYYY.mmdd
-    month = int(lastDownload)%10000 #yyyy.MMDD
-    month = int(month)/100 #MM.dd
-    day = int(lastDownload)%100 #yyyymm.DD
+    try:
+        year = abs(int(lastDownload)/10000) #YYYY.mmdd
+        month = int(lastDownload)%10000 #yyyy.MMDD
+        month = int(month)/100 #MM.dd
+        day = int(lastDownload)%100 #yyyymm.DD
 
-    lastDownload = datetime.date(int(year), int(month), int(day))
+        lastDownload = datetime.date(int(year), int(month), int(day))
+    except:
+        # We aren't able to determinate what was the last cover downloaded.
+        # starting at X days ago... Full download.
+        lastDownload = datetime.date.today() - timedelta(days=(G_TOTAL_PAGES*G_TOTAL_COVERS_PER_PAGE-1))
+        startPage = G_TOTAL_PAGES
     
+
 # starting loop until all covers are downloaded
 lackingDays = (datetime.date.today() - lastDownload).days
 for i in reversed(range(lackingDays)):
 
-    # Current page number
-    currentPage = int(i/G_TOTAL_COVERS_PER_PAGE)+1
-    row = int((G_TOTAL_COVERS_PER_PAGE*currentPage-i)/4)
-    column = 0
-    print ("page:", currentPage, " row", row, " column:", column )
+    currentPage = int(i/G_TOTAL_COVERS_PER_PAGE) # Current page number
+    coversInThisPage = i - G_TOTAL_COVERS_PER_PAGE*currentPage# total covers to download in this current page
+    row = int(coversInThisPage / G_COLUMNS_NUMBER)
+    column = coversInThisPage - row * G_COLUMNS_NUMBER
+    #print ("page:", currentPage +1, "[", row, "] [", column, "]")
 
+    coversPerPage[row][column] = datetime.date.today() - timedelta(days=i)
 
-for d1 in range(3):
-    for d2 in range(4):
-        coversPerPage[d1][d2]= d1*4 + d2
-print (coversPerPage)
+    temp = coversPerPage[row][column]
+    print ("page:", currentPage +1, "[", row, "] [", column, "] => ", temp)
+
 
